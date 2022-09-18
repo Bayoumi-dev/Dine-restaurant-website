786b91269d46b9ac7395c5e3944eaa739ae497c4
Ahmed Bayoumi <bayoumi.dev@gmail.com>
2022-09-18 21:47:02 +0200

    change root element

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
 
+document.body.insertAdjacentHTML('afterbegin', '<div id=root></div>')
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
    <React.StrictMode>
