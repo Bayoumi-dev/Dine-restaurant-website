1473860baae7880df8cbcbe187a67ecd46d459d6
Ahmed Bayoumi <bayoumi.dev@gmail.com>
2022-10-18 22:56:39 +0200

    events section(responsive on tablets)

--- src/components/EventImage.tsx
+++ src/components/EventImage.tsx
@@ -5,7 +5,9 @@ interface Props extends IEventsData {}
 
 const EventImage: FC<Props> = ({ eventsData, events }) => {
    return (
-      <div className="w-[326px] h-[400px] md:w-[573px] md:h-[360px] shadow-3xl">
+      <div
+         className="w-[326px] h-[400px] md:w-[573px] md:h-[360px] shadow-3xl relative md:after:absolute 
+      md:after:w-[160px] md:after:h-[76px] md:after:bg-pattern-lines md:after:-top-9 md:after:-left-14 ">
          <ul className="w-full h-full overflow-hidden">
             {eventsData.map((eventData, index: number) => (
                <li className="w-full h-full" key={index}>
--- src/components/EventsSection.tsx
+++ src/components/EventsSection.tsx
@@ -9,6 +9,10 @@ const EventsSection: FC = () => {
    const [events] = useState('Family Gathering')
    return (
       <section>
+         <div
+            className="w-full max-w-[1440px] h-full absolute -z-10 md:before:absolute 
+            md:before:w-[895px] md:before:h-[320px] md:before:bg-secondary-shuttleGray md:before:opacity-[0.08] 
+            md:before:rounded-tr-[100px] md:before:top-0 md:before:right-1/2 overflow-hidden"></div>
          <div className="container pt-20 pb-31 md:py-30 flex flex-col items-center text-center">
             <EventImage eventsData={eventsData} events={events} />
             <div className="w-full">
