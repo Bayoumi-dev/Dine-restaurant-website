5de16f26111ccfdbead28fc95a34f4f9bf83ad43
Ahmed Bayoumi <bayoumi.dev@gmail.com>
2022-10-26 16:51:49 +0200

    button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,21 +3,14 @@ import { FC } from 'react'
 interface Props {
    content: string
    variant?: string
+   type?: 'button' | 'submit' | 'reset'
 }
 
-const Button: FC<Props> = ({ content, variant }) => {
-   return variant === 'secondary' ? (
+const Button: FC<Props> = ({ content, variant, type }) => {
+   return (
       <button
-         className="w-[245px] h-[64px] bg-primary-codGray text-white 
-         hover:border border-primary-codGray hover:bg-white 
-        hover:text-secondary-mirage active:bg-transparent active:opacity-25">
-         {content}
-      </button>
-   ) : (
-      <button
-         className="w-[245px] h-[64px] border border-white hover:bg-white 
-        hover:text-secondary-mirage active:text-white active:bg-transparent 
-          active:opacity-25">
+         type={type && type}
+         className={`btn ${!variant ? 'primary' : 'secondary'}`}>
          {content}
       </button>
    )
