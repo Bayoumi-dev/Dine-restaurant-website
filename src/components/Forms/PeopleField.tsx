84fc282d00526fa150840720273dfa07e9260211
Ahmed Bayoumi <bayoumi.dev@gmail.com>
2022-10-27 09:23:06 +0200

    people field functionality

--- src/components/Forms/PeopleField.tsx
+++ src/components/Forms/PeopleField.tsx
@@ -1,17 +1,57 @@
-import { FC } from 'react'
+import { FC, useState } from 'react'
 
 const PeopleField: FC = () => {
+   const [people, setPeople] = useState(4)
+
+   const handlePlusBtn = (e: any) => {
+      if (people < 99) {
+         setPeople(people + 1)
+         e.target.setAttribute('aria-label', 'One has been added')
+         setTimeout(
+            () => e.target.setAttribute('aria-label', 'add one person'),
+            100
+         )
+      }
+   }
+   const handleMinusBtn = (e: any) => {
+      if (people > 1) {
+         setPeople(people - 1)
+         e.target.setAttribute('aria-label', 'One has been removed')
+         setTimeout(
+            () => e.target.setAttribute('aria-label', 'remove one person'),
+            100
+         )
+      } else {
+         e.preventDefault()
+      }
+   }
+
    return (
       <fieldset className="people" aria-label="">
          <input
+            id="people"
             type="text"
             name="people"
-            value="4 people"
+            value={people === 1 ? '1 person' : `${people} people`}
             aria-label="people"
             readOnly
          />
-         <input type="button" className="plus" />
-         <input type="button" className="minus" />
+         <input
+            type="button"
+            aria-label="add one person"
+            onClick={handlePlusBtn}
+            className={`plus ${
+               people === 99 ? '!cursor-default active:!opacity-100' : ''
+            }`}
+         />
+         <input
+            type="button"
+            aria-label="remove one person"
+            onClick={handleMinusBtn}
+            className={`minus ${
+               people === 1 ? '!cursor-default active:!opacity-100' : ''
+            }`}
+         />
       </fieldset>
    )
 }
--- src/components/ReservationForm.tsx
+++ src/components/ReservationForm.tsx
@@ -6,7 +6,10 @@ import TimeFields from './Forms/TimeFields'
 
 const ReservationForm: FC = () => {
    return (
-      <form action="#" className="booking-form mt-22 md:mt-9 xl:mt-0">
+      <form
+         action="#"
+         className="booking-form mt-22 md:mt-9 xl:mt-0"
+         aria-label="Reservations form">
          <input name="name" type="text" placeholder="name" aria-label="name" />
          <input
             name="email"
