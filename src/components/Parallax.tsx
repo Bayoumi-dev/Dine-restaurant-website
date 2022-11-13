2b57ee2bc5fa833a67c1025fd4e8f1827eda8095
Ahmed Bayoumi <bayoumi.dev@gmail.com>
2022-11-13 10:47:31 +0200

    fix parallax effect

--- src/components/Parallax.tsx
+++ src/components/Parallax.tsx
@@ -8,7 +8,11 @@ const Parallax: FC<{ children: ReactNode }> = ({ children }) => {
       let parallaxOffset = 0
       let ease = 0.75
       window.addEventListener('scroll', _ => {
-         if (window.scrollY < window.innerHeight && window.innerWidth >= 1280)
+         if (
+            window.scrollY < window.innerHeight &&
+            window.innerWidth >= 1280 &&
+            (navigator as any).deviceMemory >= 8
+         )
             parallaxOffset = window.scrollY * ease
          else if (window.innerWidth < 1280) parallaxOffset = 0
 
